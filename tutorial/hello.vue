228106a3135a68850ea301a63b454830aef8dd89
yuta <yuta@yutanoMacBook-Pro.local>
2021-06-28 12:52:28 +0900

    Implement saving text func using local storage.

--- tutorial/hello.vue
+++ tutorial/hello.vue
@@ -2,14 +2,37 @@
   <div>
     <input v-model="msg"/>
     <p class="message">メッセージ: {{ msg }}</p>
+    <input v-model="url"/>
+    <p class="url">URL: {{ url }}</p>
+    <p>
+      <button @click="persist">Save</button>
+    </p>
   </div>
 </template>
 
 <script>
 export default {
     data () {
-        return { msg: 'こんにちは！' }
+        return {
+    msg: 'こんにちは！',
+    url: 0
+        }
+  },
+  mounted() {
+    if (localStorage.msg) {
+      this.msg = localStorage.msg;
     }
+    if (localStorage.url) {   
+      this.url = localStorage.url;
+    }
+  },
+  methods: {
+    persist() {
+      localStorage.msg = this.msg;
+      localStorage.url = this.url;
+      console.log('now pretend I did more stuff...');
+    }
+  }
 }
 </script>
 
