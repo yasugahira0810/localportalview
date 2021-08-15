57567bd3407092a86af9d180a573d909447d6965
yasugahira0810 <hourou_freak@yahoo.co.jp>
2021-08-15 17:50:22 +0900

    Eliminate Magic Number from Test Cases

--- __test__/002.test.js
+++ __test__/002.test.js
@@ -1,5 +1,6 @@
 // 002 リンクをクリックすると最終アクセス日時が更新され、クリック回数が1回増える
 let page;
+const TARGET_ROW = 1; // vue-good-tableの配列番号
 
 beforeAll(async () => {
   page = await browser.newPage();
@@ -12,16 +13,16 @@ beforeAll(async () => {
 
 test('002-1 vue-good-tableの最終アクセス日時が2021/8/8 00:00:00であること', async () => {
     // vue-good-tableの最終アクセス日時を取得（URLクリック前）
-    const beforeLastAccessDate = await page.evaluate(() => document.querySelectorAll('#vgt-table > tbody > tr')[1].querySelectorAll('td > span > div')[4].innerHTML)
+    const beforeLastAccessDate = await page.evaluate((TARGET_ROW) => document.querySelectorAll('#vgt-table > tbody > tr')[TARGET_ROW].querySelectorAll('td > span > div')[4].innerHTML, TARGET_ROW)
     await expect(beforeLastAccessDate).toMatch('2021/8/8 00:00:00')
 })
 
 test('002-2 vue-good-tableの最終アクセス日時に日付が更新されていること', async () => {
     // vue-good-tableのURLをクリック
-    await page.evaluate(() => document.querySelectorAll('#vgt-table > tbody > tr')[1].querySelectorAll('td')[1].querySelectorAll('span > div > a')[0].click())
+    await page.evaluate((TARGET_ROW) => document.querySelectorAll('#vgt-table > tbody > tr')[TARGET_ROW].querySelectorAll('td')[1].querySelectorAll('span > div > a')[0].click(), TARGET_ROW)
 
     // vue-good-tableの最終アクセス日時を取得（URLクリック後）
-    const afterLastAccessDate = await page.evaluate(() => document.querySelectorAll('#vgt-table > tbody > tr')[1].querySelectorAll('td > span > div')[4].innerHTML)
+    const afterLastAccessDate = await page.evaluate((TARGET_ROW) => document.querySelectorAll('#vgt-table > tbody > tr')[TARGET_ROW].querySelectorAll('td > span > div')[4].innerHTML, TARGET_ROW)
 
     // 期待値となる実行日を取得（時刻は比較対象外のため取得していない）
     var rawDate = new Date()
@@ -35,6 +36,6 @@ test('002-3 vue-good-tableのクリック回数が901回になっていること
   // クリック自体は002-2で実施済み
 
   // vue-good-tableのクリック回数を取得（URLクリック後）
-  const clickTimes = await page.evaluate(() => document.querySelectorAll('#vgt-table > tbody > tr')[1].querySelectorAll('td > span > div')[5].innerHTML)
+  const clickTimes = await page.evaluate((TARGET_ROW) => document.querySelectorAll('#vgt-table > tbody > tr')[TARGET_ROW].querySelectorAll('td > span > div')[5].innerHTML, TARGET_ROW)
   await expect(clickTimes).toContain('901')
 })
