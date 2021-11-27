4f5cc9df57c8a957fbe956c2e945706a58718693
yasugahira0810 <hourou_freak@yahoo.co.jp>
2021-11-27 09:07:54 +0900

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,68 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="../media/images/quickbookmark_logo.png"
-          transition="scale-transition"
-          width="40"
-        />
+	<v-app>
+		<v-app-bar app color="primary" dark>
+			<div class="d-flex align-center">
+				<v-img
+					alt="Vuetify Logo"
+					class="shrink mr-2"
+					contain
+					src="../media/images/quickbookmark_logo.png"
+					transition="scale-transition"
+					width="40"
+				/>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="../media/images/quickbookmark_title.png"
-          width="250"
-        />
-      </div>
+				<v-img
+					alt="Vuetify Name"
+					class="shrink mt-1 hidden-sm-and-down"
+					contain
+					min-width="100"
+					src="../media/images/quickbookmark_title.png"
+					width="250"
+				/>
+			</div>
 
-      <v-spacer></v-spacer>
+			<v-spacer></v-spacer>
 
-<v-btn icon>
-          <v-icon>mdi-plus</v-icon>
-        </v-btn>
-  
-        <v-btn icon>
-          <v-icon>mdi-table</v-icon>
-        </v-btn>
-    </v-app-bar>
+      <v-btn icon>
+				<v-icon>mdi-table</v-icon>
+			</v-btn>
 
-    <v-main>
-      <SearchTable/>
-    </v-main>
-  </v-app>
+      <v-btn icon>
+				<v-icon>mdi-plus</v-icon>
+			</v-btn>
+
+      <v-btn icon>
+				<v-icon>mdi-arrow-collapse-down</v-icon>
+			</v-btn>
+
+			<v-btn icon>
+				<v-icon>mdi-arrow-collapse-up</v-icon>
+			</v-btn>
+
+			<v-btn icon>
+				<v-icon>mdi-information-outline</v-icon>
+			</v-btn>
+
+		</v-app-bar>
+
+		<v-main>
+			<SearchTable />
+		</v-main>
+	</v-app>
 </template>
 
 <script>
-import SearchTable from './components/SearchTable';
+import SearchTable from "./components/SearchTable";
 
 export default {
-  name: 'App',
+	name: "App",
 
-  components: {
-    SearchTable,
-  },
+	components: {
+		SearchTable
+	},
 
-  data: () => ({
-    //
-  }),
+	data: () => ({
+		//
+	})
 };
 </script>
