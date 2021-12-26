d542783cf29994d494a82c5d61393504776d85ab
yasugahira0810 <hourou_freak@yahoo.co.jp>
2021-12-26 15:45:53 +0900

    line format

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    server: {
-      command: 'BROWSER=none npm run serve',
-      port: 8080,
-    },
-  };
+  server: {
+    command: 'BROWSER=none npm run serve',
+    port: 8080,
+  },
+};
